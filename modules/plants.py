26e9d0968f84eaa2410ec06111daf92ec26fe1ec
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-04-21 17:04:38 +0200

    add image

--- modules/plants.py
+++ modules/plants.py
@@ -5,26 +5,13 @@ from firebase_admin import firestore
 import json
 import config
 import uuid
+import requests
 
 OPEN_AI_KEY = config.OPEN_AI_KEY
+PIXABAY_KEY = config.PIXABAY_KEY
 cred = credentials.Certificate(".env/firebase_key.json")
 
 firebase_admin.initialize_app(cred)
-
-def request_open_ai(text: str):
-    openai.api_key = OPEN_AI_KEY
-    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=1000)
-
-    return response["choices"][0]["text"]
-
-
-def push_plant_to_firebase(id, plant):
-    db = firestore.client()
-
-    # Add the new document to the "plants" collection
-    doc_ref = db.collection('plants').document(str(id))
-    doc_ref.set(plant)
-    return "success"
 
 def plantlookup(name: str):
     #search for Plant in Firestore
@@ -47,7 +34,7 @@ def generate_new_plant(name: str):
         "id": uuid.uuid4().hex,
         "common_name": name,
     }
-    prompt = 'Liefere mir ein Array mit Daten über: \n ' + name + '\n mit den Informationen: \n {"scientific_name": latein \n "description": ein Satz \n "harvest": Ein Wort aus Frühling, Sommer, Herbst, Winter \n "sun": ganzzahliger Wert zwischen 0 und 10 \n "water": ganzzahliger Wert zwischen 0 und 10 \n "ph": ganzzahliger Wert zwischen 0 und 14 \n "companion_plants": Aufzählung von Pflanzennamen getrennt mit einem Komma} \n Beachte die Formatierungsvorgaben nach dem jeweiligen Doppelpunkt. \n Gibt es mehrere Pflanzen mit dieser Bezeichnung wähle die am weitesten verbreitete aus. \n Liefere mir nur das Array und keine weiteren Informationen oder Text zurück.'
+    prompt = 'Liefere mir ein Array mit Daten über: \n ' + name + '\n mit den Informationen: \n {"scientific_name": latein \n "description": maximal drei Sätze \n "harvest": Ein Wort aus Frühling, Sommer, Herbst, Winter \n "sun": ganzzahliger Wert zwischen 0 und 10 \n "water": ganzzahliger Wert zwischen 0 und 10 \n "ph": ganzzahliger Wert zwischen 0 und 14 \n "companion_plants": Aufzählung von Pflanzennamen getrennt mit einem Komma} \n Beachte die Formatierungsvorgaben nach dem jeweiligen Doppelpunkt. \n Gibt es mehrere Pflanzen mit dieser Bezeichnung wähle die am weitesten verbreitete aus. \n Liefere mir nur das Array und keine weiteren Informationen oder Text zurück.'
     ai_response = request_open_ai(prompt)
     # parse the API response as a dictionary
     api_dict = json.loads(ai_response.replace('\n', '').replace('[', '').replace(']', ''))
@@ -62,7 +49,33 @@ def generate_new_plant(name: str):
         "ph": api_dict.get("ph"),
         "companion_plants": api_dict.get("companion_plants")
     })
+
+    #get plant image
+    plant["image"] = plant_image(name)
+
     #push to firebase
     push_plant_to_firebase(plant["id"], plant)
     return plant
 
+def request_open_ai(text: str):
+    openai.api_key = OPEN_AI_KEY
+    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=1000)
+
+    return response["choices"][0]["text"]
+
+def plant_image(name):
+    #search for plant image via pixabay API
+    url="https://pixabay.com/api/?key="+PIXABAY_KEY+"&q="+name+"+Pflanze&image_type=photo&category=nature&per_page=3"
+    response = requests.get(url).text
+    parsed_data = json.loads(response)
+    img_url = parsed_data["hits"][0]["webformatURL"]
+    return img_url
+
+
+def push_plant_to_firebase(id, plant):
+    db = firestore.client()
+
+    # Add the new document to the "plants" collection
+    doc_ref = db.collection('plants').document(str(id))
+    doc_ref.set(plant)
+    return "success"
