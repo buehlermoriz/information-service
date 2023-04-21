0dfee11cf5279204ea2905bd7e000001b3711f35
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-04-21 16:33:13 +0200

    tidy up

--- modules/connectors.py
+++ modules/connectors.py
@@ -30,22 +30,32 @@ def push_plant_to_firebase(id, plant):
     return "success"
 
 def plantlookup(name: str):
-    #plant lookup via treffle.io
-    # url = "https://trefle.io/api/v1/species/search?q=" + name +"&token=" + TREFLE_KEY
-    # response = requests.get(url).text
-    # parsed_data = json.loads(response)
-    # first_response = parsed_data["data"][0]
+    #search for Plant in Firestore
+    db = firestore.client()
+    docs = db.collection('plants').where('common_name', '==', name).get()
+
+    #if plant is in the database
+    if len(docs) > 0:
+        plant = docs[0].to_dict()
+        return plant
+    #if plant is nowhere in the database
+    else:
+        plant = generate_new_plant(name)
+        return plant
+    
+
+def generate_new_plant(name: str):
+    #generate and upload new plant data
     plant = {
         "id": uuid.uuid4().hex,
         "common_name": name,
     }
-    prompt = 'Liefere mir ein Array mit Daten über: \n ' + name + '\n mit den Informationen: \n "scientific_name": latein \n "description": ein Satz \n "harvest": Ein Wort aus Frühling, Sommer, Herbst, Winter \n "sun": ganzzahliger Wert zwischen 0 und 10 \n "water": ganzzahliger Wert zwischen 0 und 10 \n "ph": ganzzahliger Wert zwischen 0 und 14 \n "companion_plants": Aufzählung von Pflanzennamen getrennt mit einem Komma \n Beachte die Formatierungsvorgaben nach dem jeweiligen Doppelpunkt. \n Gibt es mehrere Pflanzen mit dieser Bezeichnung wähle die am weitesten verbreitete aus. \n Liefere mir nur das Array und keine weiteren Informationen oder Text zurück.'
+    prompt = 'Liefere mir ein Array mit Daten über: \n ' + name + '\n mit den Informationen: \n {"scientific_name": latein \n "description": ein Satz \n "harvest": Ein Wort aus Frühling, Sommer, Herbst, Winter \n "sun": ganzzahliger Wert zwischen 0 und 10 \n "water": ganzzahliger Wert zwischen 0 und 10 \n "ph": ganzzahliger Wert zwischen 0 und 14 \n "companion_plants": Aufzählung von Pflanzennamen getrennt mit einem Komma} \n Beachte die Formatierungsvorgaben nach dem jeweiligen Doppelpunkt. \n Gibt es mehrere Pflanzen mit dieser Bezeichnung wähle die am weitesten verbreitete aus. \n Liefere mir nur das Array und keine weiteren Informationen oder Text zurück.'
     ai_response = request_open_ai(prompt)
     # parse the API response as a dictionary
-    #ai_response = '\n\n[\n  "scientific_name": "Rubus fruticosus",\n  "description": "Die Brombeere ist eine krautige Pflanze, die ursprünglich aus Europa stammt und in vielen Teilen der Welt anzutreffen ist.",\n  "harvest": "Sommer",\n  "sun": 6,\n  "water": 5,\n  "ph": 6,\n  "companion_plants": "Gurke, Tomate, Paprika, Auberginen, Kohl"\n]'
     api_dict = json.loads(ai_response.replace('\n', '').replace('[', '').replace(']', ''))
 
-# update the plant dictionary with the API data
+    # update the plant dictionary with the API data
     plant.update({
         "scientific_name": api_dict.get("scientific_name"),
         "description": api_dict.get("description"),
@@ -57,7 +67,6 @@ def plantlookup(name: str):
     })
     #push to firebase
     push_plant_to_firebase(plant["id"], plant)
-
     return plant
 
 def get_lat_long(city: str):
