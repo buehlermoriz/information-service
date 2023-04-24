8b65410c885dc02cfcac23c851e8049240d87635
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-04-24 12:00:47 +0200

    minor changes

--- modules/plants.py
+++ modules/plants.py
@@ -9,7 +9,7 @@ import requests
 
 OPEN_AI_KEY = config.OPEN_AI_KEY
 PIXABAY_KEY = config.PIXABAY_KEY
-cred = credentials.Certificate(".env/firebase_key.json")
+cred = credentials.Certificate("env/firebase_key.json")
 placeholder_img = "https://pixabay.com/get/g3ea5c0ed6d9e4188af3264388e236188a0e2785f78d7d8d530b2dcb19e94a7e19ef01b6899578d6c5f76399f88c4a3d70e28f016267a7d9463c029aec451103a_640.jpg"
 
 firebase_admin.initialize_app(cred)
