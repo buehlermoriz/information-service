69740ca232a76ee3f285b8176e867b44d64127b0
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-04-24 15:47:10 +0200

    add weather

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+#from modules import plants
+from modules import weather
 
 
 app = Flask(__name__, static_folder="./templates/static")
@@ -8,5 +10,25 @@ app = Flask(__name__, static_folder="./templates/static")
 @app.route('/')
 def index():
     return "true"
+
+    
+# @app.route("/get_plant")
+# def get_plant():
+#     try:
+#         name = request.args.get('name')
+#         response = plants.plantlookup(name)
+#         return response
+#     except Exception as e:
+#         return str(e)
+    
+@app.route("/get_weather")
+def get_weather():
+    try:
+        city = request.args.get('city')
+        # if plant is nowhere in the database
+        response = weather.get_weather(city)
+        return response
+    except Exception as e:
+        return str(e)
     
 if __name__ == "__main__": app.run()
