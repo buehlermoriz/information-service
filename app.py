fba5a7b26ed74b6b1b55ec0d11b548c6d7079f36
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-04-24 15:54:08 +0200

    add plants

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-#from modules import plants
+from modules import plants
 from modules import weather
 
 
@@ -12,14 +12,14 @@ def index():
     return "true"
 
     
-# @app.route("/get_plant")
-# def get_plant():
-#     try:
-#         name = request.args.get('name')
-#         response = plants.plantlookup(name)
-#         return response
-#     except Exception as e:
-#         return str(e)
+@app.route("/get_plant")
+def get_plant():
+    try:
+        name = request.args.get('name')
+        response = plants.plantlookup(name)
+        return response
+    except Exception as e:
+        return str(e)
     
 @app.route("/get_weather")
 def get_weather():
--- modules/plants.py
+++ modules/plants.py
@@ -6,19 +6,20 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import json
-import config
 import uuid
 import requests
 
 
-OPEN_AI_KEY = config.OPEN_AI_KEY
-PIXABAY_KEY = config.PIXABAY_KEY
-cred = credentials.Certificate("env/firebase_key.json")
+# import config
+# OPEN_AI_KEY = config.OPEN_AI_KEY
+# PIXABAY_KEY = config.PIXABAY_KEY
+# cred = credentials.Certificate("env/firebase_key.json")
 
 placeholder_img = "https://pixabay.com/get/g3ea5c0ed6d9e4188af3264388e236188a0e2785f78d7d8d530b2dcb19e94a7e19ef01b6899578d6c5f76399f88c4a3d70e28f016267a7d9463c029aec451103a_640.jpg"
 
-# OPEN_AI_KEY = S3Connection(os.environ['OPEN_AI_KEY'])
-# PIXABAY_KEY = S3Connection(os.environ['PIXABAY_KEY'])
+OPEN_AI_KEY = os.environ.get('OPEN_AI_KEY')
+PIXABAY_KEY = os.environ.get('PIXABAY_KEY')
+cred = credentials.Certificate(os.environ.get('FIREBASE_KEY'))
 
 firebase_admin.initialize_app(cred)
 
