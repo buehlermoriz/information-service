eaaf840a6ef776df1627ef01af0730986f5a93c2
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-04-24 16:07:12 +0200

    Update plants.py

--- modules/plants.py
+++ modules/plants.py
@@ -23,7 +23,7 @@ FIREBASE_KEY = {
   "type": "service_account",
   "project_id": os.environ.get('project_id'),
   "private_key_id": os.environ.get('private_key_id'),
-  "private_key": os.environ.get('private_key'),
+  "private_key": os.environ.get('private_key').replace("\\n", "\n"),
   "client_email": os.environ.get('client_email'),
   "client_id": os.environ.get('client_id'),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
