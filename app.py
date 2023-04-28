26cf22295b259e273dc0c55b87a221e134355c64
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2023-04-28 15:49:52 +0200

    change status endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from modules import plants
 from modules import weather
 import os
@@ -10,7 +10,7 @@ app = Flask(__name__, static_folder="./templates/static")
 #url-routes 
 @app.route('/')
 def index():
-    return "false"
+    return jsonify({"online": True}), 200
 
     
 @app.route("/get_plant", methods=['GET', 'POST'])
